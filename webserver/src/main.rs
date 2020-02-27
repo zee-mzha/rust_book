ed200a60a9beeb383a2f957abaae987127b54b1a
zee.mzha <zee.mzha@gmail.com>
2020-02-26 18:28:52 -0700

    don't panic if creating server fails, report error and exit

--- webserver/src/main.rs
+++ webserver/src/main.rs
@@ -1,8 +1,15 @@
 mod server;
 use server::Server;
 use std::env;
+use std::process;
 
 fn main(){
-	let mut server = Server::new(env::args()).unwrap();
+	let mut server = match Server::new(env::args()){
+		Ok(s) => s,
+		Err(e) => {
+			println!("Failed to start server: {}", e);
+			process::exit(1);
+		}
+	};
 	server.run();
 }
